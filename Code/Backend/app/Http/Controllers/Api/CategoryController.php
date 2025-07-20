6e22929b094640679975433f71902f13d1ad4321
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-07-20 23:46:20 +0100

    feat: Implementer la méthode 'addCategory' dans 'CategoryController'

--- Code/Backend/app/Http/Controllers/Api/CategoryController.php
+++ Code/Backend/app/Http/Controllers/Api/CategoryController.php
@@ -3,9 +3,39 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\CategoryRequest;
+use App\Services\CategoryService;
+use Illuminate\Validation\ValidationException;
 
 class CategoryController extends Controller
 {
-    //
+    protected $CatServ;
+    
+    public function __construct(CategoryService $CatServ) { $this->CatServ = $CatServ; }
+
+    public function addCategory(CategoryRequest $category)
+    {
+        try {
+            $result = $this->CatServ->addCategory($category->validated());
+            return response()->json([
+                'message' => 'Catégorie ajoutée avec succès',
+                'data' => $result,
+                'status' => 'success'
+            ], 201);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'message' => 'Erreur de validation',
+                'erreur' => $e->errors(),
+                'status' => 'failed'
+            ], 422);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Erreur inattendue',
+                'erreur' => $e->getMessage(),
+                'status' => 'failed'
+            ], 500);
+        }
+    }
+
+
 }
