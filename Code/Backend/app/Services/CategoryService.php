3bf4a46b5db36dd74402a2cf40856681d827a4b5
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-07-21 00:23:20 +0100

    refact: Nettoyage du 'CategoryService'

--- Code/Backend/app/Services/CategoryService.php
+++ Code/Backend/app/Services/CategoryService.php
@@ -8,7 +8,7 @@ use App\Repositories\CategoryRepo;
 class CategoryService
 {
     protected $catRepo;
-    public function __construct(CategoryInterface $catRepo) { return $this->catRepo = $catRepo; }
+    public function __construct(CategoryInterface $catRepo) { $this->catRepo = $catRepo; }
 
     public function addCategory($category) { return $this->catRepo->addCategory($category); }
     public function updateCategoryName($categoryId, $newCategoryName) { return $this->catRepo->updateCategoryName($categoryId, $newCategoryName); }
