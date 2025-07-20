11b812b26c961f8e8b8973454ff668ab952cb0de
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-07-20 22:22:11 +0100

    feat: Ajouter 'CategoryRepo' avec les méthode de 'CategoryInterface' vide

---
+++ Code/Backend/app/Repositories/CategoryRepo.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\CategoryInterface;
+
+class CategoryRepo implements CategoryInterface
+{
+    public function addCategory($category)
+    {
+        
+    }
+
+    public function updateCategoryName($newCategoryName)
+    {
+        
+    }
+
+    public function deleteCategory($category, $deletType)
+    {
+        
+    }
+}
