b7d1b60cadb69ba5575c64771caa5ba3cdc4f306
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-07-21 00:25:27 +0100

    feat: Ajouter les routes vers les méthodes du 'CategoryController'

--- Code/Backend/routes/api.php
+++ Code/Backend/routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\CategoryController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::controller(CategoryController::class)->group(function(){
+    Route::post('/categories', 'addCategory');
+    Route::patch('/categories/{categoryId}/name', 'updateCategoryName');
+    Route::delete('/categories/{categoryId}', 'deleteCategory');
+});
