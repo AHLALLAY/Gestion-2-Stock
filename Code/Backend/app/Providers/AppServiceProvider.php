4680585b2fd9b82aed6e1b03237c5c6ef5aa4259
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-07-20 22:50:01 +0100

    feat: Liée entre 'CategoryInterface' et 'CategoryRepo' dans 'AppServiceProvider'

--- Code/Backend/app/Providers/AppServiceProvider.php
+++ Code/Backend/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Interfaces\CategoryInterface;
+use App\Repositories\CategoryRepo;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(CategoryInterface::class, CategoryRepo::class);
     }
 
     /**
