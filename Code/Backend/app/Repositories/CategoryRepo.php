137bba00fd02caab7a4c76517a0c271448a36536
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-07-20 22:25:34 +0100

    feat: Implementer 'addCategory' dans 'CategoryRepo'

--- Code/Backend/app/Repositories/CategoryRepo.php
+++ Code/Backend/app/Repositories/CategoryRepo.php
@@ -3,12 +3,17 @@
 namespace App\Repositories;
 
 use App\Interfaces\CategoryInterface;
+use App\Models\Category;
 
 class CategoryRepo implements CategoryInterface
 {
     public function addCategory($category)
     {
-        
+        try{
+            return Category::create($category);
+        }catch(\Exception $e){
+            return $e->getMessage();
+        }
     }
 
     public function updateCategoryName($newCategoryName)
