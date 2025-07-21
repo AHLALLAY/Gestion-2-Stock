4d65306042f378b53cb584acf27b13f6e23da349
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-07-21 18:28:27 +0100

    refact: Renommer la migration 'categories'

--- Code/Backend/database/migrations/2025_07_20_210449_create_brands_table.php
+++ Code/Backend/database/migrations/2025_07_20_210449_create_brands_table.php
@@ -8,7 +8,7 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('brands', function (Blueprint $table) {
             $table->id();
             $table->string("name");
             $table->boolean("is_deleted")->default(false);
@@ -19,6 +19,6 @@ return new class extends Migration
 
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('brands');
     }
 };
