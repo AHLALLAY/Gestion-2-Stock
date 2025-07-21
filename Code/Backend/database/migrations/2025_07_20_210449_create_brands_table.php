c8112159b85649c883b76f0296da8e2ede6d0496
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-07-21 20:21:29 +0100

    feat: Ajouter la constraint 'unique' dans la migration 'brands'

--- Code/Backend/database/migrations/2025_07_20_210449_create_brands_table.php
+++ Code/Backend/database/migrations/2025_07_20_210449_create_brands_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
     {
         Schema::create('brands', function (Blueprint $table) {
             $table->id();
-            $table->string("name");
+            $table->string("name")->unique();
             $table->boolean("is_deleted")->default(false);
             $table->timestamps();
         });
