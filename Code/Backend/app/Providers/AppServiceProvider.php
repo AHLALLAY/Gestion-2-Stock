ea7dc9eedb74507c83187451b318ae34a6499e29
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-07-22 20:19:39 +0100

    feat: Liée entre l'interface de produit et statistiques et leur repositorie

--- Code/Backend/app/Providers/AppServiceProvider.php
+++ Code/Backend/app/Providers/AppServiceProvider.php
@@ -4,8 +4,12 @@ namespace App\Providers;
 
 use App\Interfaces\BrandInterface;
 use App\Interfaces\ModelInterface;
+use App\Interfaces\ProductInterface;
+use App\Interfaces\StatistiqueInterface;
 use App\Repositories\BrandRepo;
 use App\Repositories\ModelRepo;
+use App\Repositories\ProductRepo;
+use App\Repositories\StatistiqueRepo;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->app->bind(BrandInterface::class, BrandRepo::class);
         $this->app->bind(ModelInterface::class, ModelRepo::class);
+        $this->app->bind(ProductInterface::class, ProductRepo::class);
+        $this->app->bind(StatistiqueInterface::class, StatistiqueRepo::class);
 
     }
 
