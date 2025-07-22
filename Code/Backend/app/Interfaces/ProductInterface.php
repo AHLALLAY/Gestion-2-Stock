5d0a6ab8a05f360bfe9fe8f0e536649f0b1584d7
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-07-22 15:13:40 +0100

    refact: Déplacer les signature des statistique vers 'StatistiqueInterface'

--- Code/Backend/app/Interfaces/ProductInterface.php
+++ Code/Backend/app/Interfaces/ProductInterface.php
@@ -8,10 +8,4 @@ interface ProductInterface
     public function addProduct($product);
     public function editProduct($data, $productId);
     public function deleteProduct($productId, $typeDelete);
-
-    // statistics
-    public function getProductTotal();
-    public function getProductByModel($model);
-    public function getProductByBrand($brand);
-
 }
