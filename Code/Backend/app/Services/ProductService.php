cf8494bb4de05f2d7a6c30f288f42f565e2ff833
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-07-24 22:29:11 +0100

    feat: Déplacer les méthodes du statistiques vers 'StatisticService' après son création

--- Code/Backend/app/Services/ProductService.php
+++ Code/Backend/app/Services/ProductService.php
@@ -11,9 +11,6 @@ class ProductService
 
     public function displayProducts() { return $this->productRepo->displayProducts(); }
     public function addProduct($product) { return $this->productRepo->addProduct($product); }
-    public function editProduct($productId) { return $this->productRepo->editProduct($productId); }
+    public function editProduct($data, $productId) { return $this->productRepo->editProduct($data, $productId); }
     public function deleteProduct($productId, $typeDelete) { return $this->productRepo->deleteProduct($productId, $typeDelete); }
-    public function getProductTotal() { return $this->productRepo->getProductTotal(); }
-    public function getProductByModel($model) { return $this->productRepo->getProductByModel($model); }
-    public function getProductByBrand($brand) { return $this->productRepo->getProductByBrand($brand); }
 } 
